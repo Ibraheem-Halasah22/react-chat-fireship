dd6c6dc8ee2e6e9f61d92ca4757f52d85f3f314d
Ibraheem-Halasah22 <huyyyy524@gmail.com>
2023-11-21 20:10:43 +0300

    authentication login with Google added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -18,11 +20,30 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
-      <header></header>
+      <header>
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
+      </header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
     </div>
   );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+function SignOut() {
+  return auth.currentUser && <button>Sign out</button>;
+}
+function ChatRoom() {
+  return <div>Chat Room</div>;
 }
 
 export default App;
